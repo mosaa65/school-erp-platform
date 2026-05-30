07b332ea1a5cc9a0b353bc1221a2db8ce734d61e
MOUSA GAMIL AL-AWADHI <157767146+mosaa65@users.noreply.github.com>
2026-05-30 23:51:39 +0300

    feat: إضافة Google AdSense Script في layout.tsx باستخدام next/script لدعم الإعلانات

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,7 +1,8 @@
-﻿import type { Metadata } from "next";
+import type { Metadata } from "next";
 import { Almarai, Cairo, JetBrains_Mono, Noto_Kufi_Arabic, Tajawal } from "next/font/google";
 import { AppProviders } from "@/components/providers/app-providers";
 import "./globals.css";
+import Script from "next/script";
 
 const cairo = Cairo({
   subsets: ["arabic", "latin"],
@@ -49,13 +50,15 @@ export default function RootLayout({
       suppressHydrationWarning
       className={`${cairo.variable} ${jetbrainsMono.variable} ${tajawal.variable} ${notoKufiArabic.variable} ${almarai.variable}`}
     >
+      <Script
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1544047732487982"
+        strategy="afterInteractive"
+        crossOrigin="anonymous"
+        async
+      />
       <body className="min-h-screen">
         <AppProviders>{children}</AppProviders>
       </body>
     </html>
   );
 }
-
-
-
-
